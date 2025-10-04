568c58793dbaf0018630b8bcac0783aa6a20fffe
Ludovic Guegan <ludovic.guegan@gmail.com>
2025-10-05 00:01:57 +0100

    Fix Press to continue in congrats screen #4

--- cmd/essentialist/congrats.go
+++ cmd/essentialist/congrats.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
@@ -17,6 +18,23 @@ func NewCongratsScreen(game *internal.Game) Screen {
 	return &CongratsScreen{game: game}
 }
 
+func (s *CongratsScreen) keyHandler(app Application) func(*fyne.KeyEvent) {
+	return func(key *fyne.KeyEvent) {
+		if key.Name != "" {
+			switch key.Name {
+			case fyne.KeyQ, fyne.KeyEscape, fyne.KeyReturn, fyne.KeySpace:
+				app.Display(NewSplashScreen())
+			}
+		} else {
+			switch key.Physical {
+			case fyne.HardwareKey{ScanCode: 9}, fyne.HardwareKey{ScanCode: 24},
+				fyne.HardwareKey{ScanCode: 36}, fyne.HardwareKey{ScanCode: 55}: // Enter or Space
+				app.Display(NewSplashScreen())
+			}
+		}
+	}
+}
+
 func (s *CongratsScreen) Show(app Application) {
 	topBar := newProgressTopBar(app, s.game)
 	label := container.New(layout.NewCenterLayout(),
@@ -28,7 +46,7 @@ func (s *CongratsScreen) Show(app Application) {
 	box := container.New(layout.NewBorderLayout(topBar, button, nil, nil),
 		topBar, button, label)
 	app.SetContent(box)
-	app.SetOnTypedKey(EscapeKeyHandler(app, NewSplashScreen()))
+	app.SetOnTypedKey(s.keyHandler(app))
 }
 
 func (s *CongratsScreen) Hide(app Application) {
